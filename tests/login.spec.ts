16edc9a70c96a566ec21c7336838da38820985ff
Max <awesomemg@hotmail.co.uk>
2026-03-26 23:06:46 +0000

    Initial commit - login test with modal handling

---
+++ tests/login.spec.ts
@@ -0,0 +1,20 @@
+import { test, expect } from '@playwright/test';
+
+test('User can successfully login using valid credentials', async ({ page }) => {
+  const limitedFunctionalities = page.locator('.modal-body');
+  await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+  //Fill in correct username and password
+  await page.getByLabel(/Username/).fill('rahulshettyacademy');
+  await page.getByLabel(/Password/).fill('Learning@830$3mK2');
+  //Mark user is not an admin user
+  await page.locator('.checkmark').last().click();
+  //Pop-up appears saying some permissions are restricted to admin users, click okay
+  await expect(limitedFunctionalities).toBeVisible();
+  await page.getByRole('button', { name:'Okay'}).click();
+  await page.locator('select.form-control').selectOption('Consultant');
+  await page.getByRole('checkbox', {name: 'I Agree to the terms and conditions'}).click();
+  await page.locator('signInBtn').click();
+  await page.pause();
+  await page.goto("https://rahulshettyacademy.com/angularpractice/shop");
+  console.log(await expect(page).toHaveTitle(''));
+});
