6332806732d10d192436a35c8a263fc9b32362b7
Max <awesomemg@hotmail.co.uk>
2026-03-29 20:56:34 +0100

    enhanced assertions for test focusing on adding items to cart

--- tests/add-items-to-cart-and-checkout.spec.ts
+++ tests/add-items-to-cart-and-checkout.spec.ts
@@ -2,8 +2,9 @@ import { test, expect } from '@playwright/test';
 
 test('User can successfully add items to cart and check out', async ({ page }) => {
   const limitedFunctionalities = page.locator('.modal-body');
-  const iphonex = page.getByText('iphone X');
-  const purchaseSuccess = page.getByText('Success! Thank you! Your order will be delivered in next few weeks :-).');
+  const iphoneCard = page.locator('app-card').filter({
+    has: page.getByText('iphone X', { exact: true })
+  });
   await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
   await page.getByLabel(/Username/).fill('rahulshettyacademy');
   await page.getByLabel(/Password/).fill('Learning@830$3mK2');
@@ -15,15 +16,17 @@ test('User can successfully add items to cart and check out', async ({ page }) =
   await page.locator('#signInBtn').click();
   await expect(page).toHaveTitle('ProtoCommerce');
 
-  await expect(iphonex).toBeVisible();
-  await page.getByRole('button', { name: 'Add' }).first().click();
+
+  await expect(iphoneCard).toHaveCount(1);
+  await iphoneCard.getByRole('button', { name: 'Add' }).click();
   await page.getByText('Checkout ( 1 )').click();
-  await page.getByText('₹. 100000');
+  await expect(page.getByText('In Stock')).toBeVisible();
+  await expect(page.locator('tr').filter({hasText: 'Total ₹.'})).toBeVisible();
   await page.getByRole('button', { name: 'Checkout' }).click();
-  await page.getByText('Please choose your delivery location.');
+  await expect(page.getByText('Please choose your delivery location.')).toBeVisible();
   await page.locator('#country').fill('Oxford, United Kingdom');
   await page.getByText(/term & Conditions/i).click();
   await page.getByText('Close').click();
-  await page.getByRole('button', {name: 'Purchase'}).click();
-  await expect(purchaseSuccess).toBeVisible();
+  await page.getByRole('button', { name: 'Purchase' }).click();
+  await expect(page.getByText('Success! Thank you! Your order will be delivered in next few weeks :-).')).toBeVisible();
 });
